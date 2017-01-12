d85243086212d6c278c57e4120a86f5178bbe728
Javier Pallarés <javpalsaa@alum.us.es>
2017-01-12 01:14:11 +0100

    Añadido test que prueba la resta de tres números

--- src/test/RestaTest.java
+++ src/test/RestaTest.java
@@ -12,5 +12,10 @@ public class RestaTest {
 	public void restaTest2() {
 		assertEquals(4, Resta.resta(7, 3));
 	}
+	
+	@Test
+	public void restaTest3() {
+		assertEquals(-1, Resta.resta(7, 3, 5));
+	}
 
 }
